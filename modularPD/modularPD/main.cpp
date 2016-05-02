5d7cc787d0a04846c4cc95fa6f629fd7c91fec24
Javier Fernández Marqués <jafermarq@gmail.com>
2016-05-02 11:02:11 +0100

    First commit. Excluded all files (via .gitignore) except ".cpp", ".h" and Xcode project properties files "project.pbxproj"

---
+++ modularPD/modularPD/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  modularPD
+//
+//  Created by Javier Fernández Marqués on 02/05/2016.
+//  Copyright © 2016 Javier Fernandez Marques. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv2/core/core.hpp>
+
+int main(int argc, const char * argv[]) {
+
+    cv::Mat a = cv::Mat::eye(5, 5, CV_8U);
+    
+    std::cout << a << std::endl;
+}
